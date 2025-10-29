f178ed04a53f40f664b4ddcd0eff7a386b75581a
felixdev <sungasfa@gmail.com>
2025-10-29 17:34:36 -0400

    Crear sockets-delprimary.js

---
+++ plugins/sockets-delprimary.js
@@ -0,0 +1,35 @@
+import ws from 'ws'
+
+const handler = async (m, { conn, usedPrefix }) => {
+  const chat = global.db.data.chats[m.chat]
+
+  if (!chat.primaryBot) {
+    return conn.reply(m.chat, `*🙁 Todos los magos pueden usar su magia aquí porque no hay ninguno establecido como principal.*`, m, rcanal)
+  }
+
+  try {
+    const oldPrimary = chat.primaryBot
+    chat.primaryBot = null
+
+    conn.reply(
+      m.chat, 
+      `*🌟 El mago @${oldPrimary.split`@`[0]} se le acabo su poder. Ahora todos los magos pueden usar su magia con facilidad.*`, 
+      m, 
+      { mentions: [oldPrimary] }
+    )
+  } catch (e) {
+    conn.reply(
+      m.chat, 
+      `*😿 No pude hacerlo. disculpa.*`, 
+      m
+    )
+  }
+}
+
+handler.help = ['delprimary']
+handler.tags = ['grupo']
+handler.command = ['delprimary']
+handler.group = true
+handler.admin = true  
+
+export default handler
