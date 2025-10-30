73b8cc157531f391410eb437d2b70a95d03e4144
felixdev <sungasfa@gmail.com>
2025-10-30 06:24:29 -0400

    Actualizar sockets-leave.js

--- plugins/sockets-leave.js
+++ plugins/sockets-leave.js
@@ -1,13 +1,24 @@
-// commands/leave.js
+const handler = async (m, { conn, command, usedPrefix, text }) => {
+  try {
+    const isSubBots = [conn.user.jid, ...global.owner.map(([number]) => `${number}@s.whatsapp.net`)].includes(m.sender)
+    if (!isSubBots) return m.reply(`🤨 Eso solo puede ser usado por mi we`)
 
-const handler = async (m, { conn, isOwner }) => {
-  if (!isOwner) return;
-  if (!m.isGroup) return;
-  await conn.sendMessage(m.chat, { text: '*👋 Saliendo del grupo...*' }, { quoted: m });
-  await conn.groupLeave(m.chat);
-};
+    await m.react('🕒')
+    const id = text || m.chat
+    const chat = global.db.data.chats[m.chat]
+    chat.welcome = false
+    await conn.reply(id, `☹ Adiós, La bot más magica se despide de todos`)
+    await conn.groupLeave(id)
+    chat.welcome = true
+    await m.react('✔️')
+  } catch (error) {
+    await m.react('✖️')
+    conn.reply(m.chat, `Error:\n\n${error.message || error}`, m, rcanal)
+  }
+}
 
-handler.command = ['leave'];
-handler.owner = true;
+handler.command = ['leave']
+handler.help = ['leave']
+handler.tags = ['socket']
 
-export default handler;
+export default handler
