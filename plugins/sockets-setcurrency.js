88379e3ac11568b4cbc4d960ffac8b56d7c8faff
PixelCrew Team <pixelcrewteam@gmail.com>
2025-10-30 16:31:43 -0400

    Actualizar sockets-setcurrency.js

--- plugins/sockets-setcurrency.js
+++ plugins/sockets-setcurrency.js
@@ -2,42 +2,41 @@ import fs from 'fs'
 import path from 'path'
 
 const handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) return m.reply(`👑 Formato erróneo. Cambia mi moneda asi:\n\n- #setcurrency + <nueva_moneda>`)
+  if (!text) return m.reply(`👑 Formato erróneo. Cambia mi currency asi:\n\n- #setcurrency + <nueva_moneda>`)
 
   const senderNumber = m.sender.replace(/[^0-9]/g, '')
   const botPath = path.join('./JadiBots', senderNumber)
   const configPath = path.join(botPath, 'config.json')
 
+  // Verifica que la sesión del bot exista (solo el dueño de esa sesión puede modificarla)
   if (!fs.existsSync(botPath)) {
-    return m.reply('❖ El comando *setname* solo puede ser usado por el dueño del número del *bot.*')
+    return m.reply('❖ El comando *setcurrency* solo puede ser usado por el dueño del número del *bot.*')
   }
 
   let config = {}
-
 
   if (fs.existsSync(configPath)) {
     try {
       config = JSON.parse(fs.readFileSync(configPath))
     } catch (e) {
-      return m.reply('😔 *Error* al leer la moneda.')
+      return m.reply('😔 *Error* al leer la configuración.')
     }
   }
-
 
   config.currency = text.trim()
 
   try {
     fs.writeFileSync(configPath, JSON.stringify(config, null, 2))
-    m.reply(`💜 La moneda de tu session fue actualizada a *${text.trim()}*`)
+    m.reply(`💜 La currency de tu session fue actualizada a *${text.trim()}*`)
   } catch (err) {
     console.error(err)
-    m.reply('😔 Ocurrió un error al guardar el nombre.')
+    m.reply('😔 Ocurrió un error al guardar la currency.')
   }
 }
 
 handler.help = ['setcurrency']
-handler.tags= ['sockets']
+handler.tags = ['sockets']
 handler.command = /^setcurrency$/i
-handler.owner = false // solo el dueño puede usar esto
+handler.owner = true // solo el dueño puede usar esto
 
 export default handler
