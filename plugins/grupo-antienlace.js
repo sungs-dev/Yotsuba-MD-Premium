71b7285280ff3ed9ed09b7aca6db39b30bd88333
felixdev <sungasfa@gmail.com>
2025-10-30 01:02:40 -0400

    Actualizar grupo-antienlace.js

--- plugins/grupo-antienlace.js
+++ plugins/grupo-antienlace.js
@@ -11,18 +11,18 @@ export async function before(m, { conn, isAdmin, isBotAdmin }) {
   // Gestión de comando activar/desactivar anti-enlace
   if (/^#anti(enlace|link)(\s)?(on|off)?$/i.test(m.text.trim())) {
     if (!isAdmin) {
-      await conn.reply(m.chat, `🤨 Solo los administradores pueden usar este comando.`, m);
+      await conn.reply(m.chat, `${emoji} Solo los administradores pueden usar este comando.`, m);
       return !0;
     }
     const arg = m.text.trim().split(/\s+/)[1]?.toLowerCase();
     if (arg === 'on') {
       chat.antilink = true;
-      await conn.reply(m.chat, `👑 El *Anti-enlace* ha sido activado.`, m);
+      await conn.reply(m.chat, `${emoji} El *Anti-enlace* ha sido activado.`, m);
     } else if (arg === 'off') {
       chat.antilink = false;
-      await conn.reply(m.chat, `👑 El *Anti-enlace* ha sido desactivado.`, m);
+      await conn.reply(m.chat, `${emoji} El *Anti-enlace* ha sido desactivado.`, m);
     } else {
-      await conn.reply(m.chat, `👑 Usa: #antilink on | #antilink off`, m);
+      await conn.reply(m.chat, `${emoji} Usa: #antilink on | #antilink off`, m);
     }
     return !0;
   }
@@ -40,7 +40,7 @@ export async function before(m, { conn, isAdmin, isBotAdmin }) {
       await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.key.id, participant: m.key.participant }});
     }
     // Advierte al usuario
-    await conn.reply(m.chat, `👑 El \`Anti-enlace\` está activado, no compartas enlaces si no quieres que un administrador te eliminé.`, m);
+    await conn.reply(m.chat, `${emoji} El \`Anti-enlace\` está activado, no compartas enlaces si no quieres que un administrador te eliminé.`, m);
     return !0;
   }
 } 
