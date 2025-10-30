498b0571a8df31530f5228ac1f8c275e9d35bce4
felixdev <sungasfa@gmail.com>
2025-10-30 06:11:42 -0400

    Actualizar sockets-logout.js

--- plugins/sockets-logout.js
+++ plugins/sockets-logout.js
@@ -1,12 +1,37 @@
-// commands/logout.js
+import { jidDecode } from '@whiskeysockets/baileys'
+import path from 'path'
+import fs from 'fs'
 
-const handler = async (m, { conn, isOwner }) => {
-  if (!isOwner) return;
-  await conn.sendMessage(m.chat, { text: '*👋 Cerrando sesión...*' }, { quoted: m });
-  await conn.logout();
-};
+const handler = async (m, { conn, command, usedPrefix }) => {
+  try {
+    const isSubBots = [conn.user.jid, ...global.owner.map(([number]) => `${number}@s.whatsapp.net`)].includes(m.sender)
+    if (!isSubBots) return m.reply(`🤨 Esto solo puede ser usado por mi we`)
 
-handler.command = ['logout'];
-handler.owner = true;
+    const rawId = conn.user?.id || ''
+    const cleanId = jidDecode(rawId)?.user || rawId.split('@')[0]
+    const index = global.conns?.findIndex(c => c.user.jid === m.sender)
+    if (global.conn.user.jid === conn.user.jid)
+      return conn.reply(m.chat, '👑 Este comando está deshabilitado en las sesiones principales.', m, rcanal)
+    if (index === -1 || !global.conns[index])
+      return conn.reply(m.chat, '💜 La sesión ya está cerrada o no se encontró una conexión activa.', m, rcanal)
+    conn.reply(m.chat, '😔 Tu sesión ha sido cerrada exitosamente.', m, rcanal)
+    setTimeout(async () => {
+      await global.conns[index].logout()
+      global.conns.splice(index, 1)
+      const sessionPath = path.join(global.jadi, cleanId)
+      if (fs.existsSync(sessionPath)) {
+        fs.rmSync(sessionPath, { recursive: true, force: true })
+        console.log(`La session de ${cleanId} eliminada de ${sessionPath}`)
+      }
+    }, 3000)
+  } catch (error) {
+    await m.react('✖️')
+    conn.reply(m.chat, `Error:\n\n${error.message || error}`, m, rcanal)
+  }
+}
 
-export default handler;
+handler.command = ['logout']
+handler.help = ['logout']
+handler.tags = ['socket']
+
+export default handler
