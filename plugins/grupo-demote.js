b94c25fe54ef7bb45eecb84a81819a792eac4798
felixdev <sungasfa@gmail.com>
2025-10-30 09:51:36 -0400

    Actualizar grupo-demote.js

--- plugins/grupo-demote.js
+++ plugins/grupo-demote.js
@@ -1,35 +1,36 @@
-let handler = async (m, { conn,usedPrefix, command, text }) => {
-if(isNaN(text) && !text.match(/@/g)){
+const handler = async (m, { conn }) => {
+  try {
+    // let who = m?.message?.extendedTextMessage?.contextInfo?.participant || m?.mentionedJid[0] || await m?.quoted?.sender;
+   let texto = await m.mentionedJid
+   let who = texto.length > 0 ? texto[0] : (m.quoted ? await m.quoted.sender : false)
+    if (!who) return m.reply('*ᐛ👑* Mensiona a un ciudadano de este mundo mágico para quitarle sus *privilegios altos.*');
 
-}else if(isNaN(text)) {
-var number = text.split`@`[1]
-}else if(!isNaN(text)) {
-var number = text
-}
-if(!text && !m.quoted) return conn.reply(m.chat, `*ᐛ👑* Mensiona a un ciudadano de este mundo mágico para quitarle sus *privilegios altos.*`, m, fake)
-if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `*ᐛ👑* Mensiona a un ciudadano de este mundo mágico para quitarle sus *privilegios altos.*`, m, fake)
-try {
-if(text) {
-var user = number + '@s.whatsapp.net'
-} else if(m.quoted.sender) {
-var user = m.quoted.sender
-} else if(m.mentionedJid) {
-var user = number + '@s.whatsapp.net'
-} 
-} catch (e) {
-} finally {
-conn.groupParticipantsUpdate(m.chat, [user], 'demote')
-await conn.reply(m.chat, `*ᐛ👑* El ciudadano fue mandado a los *terrenos bajos.*`, m, fake)
-await m.react('💟')
-}
+    const groupMetadata = await conn.groupMetadata(m.chat);
+    const participant = groupMetadata.participants.find(participant => participant.jid === who);
 
-}
-handler.help = ['demote *@tag*']
-handler.tags = ['grupo']
-handler.command = ['demote', 'degradar'] 
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-handler.fail = null
+    if (!participant || !participant.admin) {
+    return conn.reply(m.chat, `🤨 *@${who.split('@')[0]}* no es administrador del grupo!`, m, { mentions: [who] });
+    }
 
-export default handler
+    if (who === groupMetadata.owner) {
+      return m.reply('🤨 ¿Quieres que te quite admin?');
+    }
+
+    if (who === conn.user.jid) {
+      return m.reply('😒 No puedo quitarme admin yo misma we');
+    }
+
+    await conn.groupParticipantsUpdate(m.chat, [who], 'demote');
+    await conn.reply(m.chat, `*@${who.split('@')[0]}* Ya no es admin`, m, { mentions: [who] });
+  } catch (e) {
+    await m.reply(`Error`);
+  }
+};
+
+handler.help = ['demote'];
+handler.tags = ['grupo'];
+handler.command = ['demote'];
+handler.admin = true;
+handler.botAdmin = true;
+
+export default handler;
