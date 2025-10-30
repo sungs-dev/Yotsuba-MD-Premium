f519ed26bf2a082c8151f222c7e40b0a5a547913
felixdev <sungasfa@gmail.com>
2025-10-30 09:37:40 -0400

    Actualizar sockets-sologp.js

--- plugins/sockets-sologp.js
+++ plugins/sockets-sologp.js
@@ -15,7 +15,7 @@ const handler = async (m, { conn, command, usedPrefix, text }) => {
       config[type] = enable
       return conn.reply(m.chat, `👑 Has *${enable ? 'activado' : 'desactivado'}* el modo *${type}* para esta session.`)
     }
-    conn.reply(m.chat, `「✦」Estado del modo: *${isEnable ? '✓ Activado' : '✗ Desactivado'}* , puedes activar o desactivar usando:\n\n- >${type} + on/off>`, m, rcanal)
+    conn.reply(m.chat, `👑 Puedes activar o desactivar la función *${command}* utilizando:\n\n💜 *${command}* enable\n💜 *${command}* disable\n\n🛠 Estado actual » *${isEnable ? '✓ Activado' : '✗ Desactivado'}*`, m, rcanal)
   } catch (error) {
     await m.react('✖️')
     conn.reply(m.chat, `Error: \n\n${error.message || error}`, m, rcanal)
