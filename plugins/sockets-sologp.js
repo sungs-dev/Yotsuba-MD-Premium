518bcfd03de11220851befd22d013706373b55d1
felixdev <sungasfa@gmail.com>
2025-10-30 01:42:19 -0400

    Actualizar sockets-sologp.js

--- plugins/sockets-sologp.js
+++ plugins/sockets-sologp.js
@@ -1,7 +1,7 @@
 const handler = async (m, { conn, command, usedPrefix, text }) => {
   try {
     const isSubBots = [conn.user.jid, ...global.owner.map(([number]) => `${number}@s.whatsapp.net`)].includes(m.sender)
-    if (!isSubBots) return m.reply(`❀ El comando *${command}* solo puede ser ejecutado por el Socket.`)
+    if (!isSubBots) return m.reply(`😐 Solo yo puedo ejecutar esto we`, m, rcanal)
 
     const config = global.db.data.settings[conn.user.jid]
     const value = text ? text.trim().toLowerCase() : ''
@@ -11,7 +11,7 @@ const handler = async (m, { conn, command, usedPrefix, text }) => {
     const disable = value === 'disable' || value === 'off'
     if (enable || disable) {
       if (isEnable === enable)
-        return m.reply(`😐 El modo *${type}* ya estaba ${enable ? 'activado' : 'desactivado'} we.`, rcanal)
+        return m.reply(`😐 El modo *${type}* ya estaba ${enable ? 'activado' : 'desactivado'} we.`, m, rcanal)
       config[type] = enable
       return conn.reply(m.chat, `👑 Has *${enable ? 'activado' : 'desactivado'}* el modo *${type}* para esta session.`, m, rcanal)
     }
