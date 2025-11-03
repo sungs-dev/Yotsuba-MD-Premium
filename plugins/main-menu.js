e3dba518ff0c27daeecb64de42d96495db125202
PixelCrew Team <pixelcrewteam@gmail.com>
2025-11-03 19:38:33 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -122,9 +122,204 @@ let handler = async (m, { conn }) => {
   } catch (e) { rankText = 'N/A' }
 
   // construir texto según el template proporcionado
-  let txt = `*😿Error:*
+  let txt = `Hola, esta es la lista de los países de mierda que odio:
 
-- El menú está en proceso de reconstrucción, crea un bot tu o chupame la ñema hijo de perra.
+- Afganistán
+- Albania
+- Alemania
+- Andorra
+- Angola
+- Antigua y Barbuda
+- Arabia Saudita
+- Argelia
+- Argentina
+- Armenia
+- Australia
+- Austria
+- Azerbaiyán
+- Bahamas
+- Bangladés
+- Barbados
+- Baréin
+- Bélgica
+- Belice
+- Benín
+- Bielorrusia
+- Bolivia
+- Bosnia y Herzegovina
+- Botsuana
+- Brasil
+- Brunéi
+- Bulgaria
+- Burkina Faso
+- Burundi
+- Bután
+- Cabo Verde
+- Camboya
+- Camerún
+- Canadá
+- Catar
+- Chad
+- Chile
+- China
+- Chipre
+- Ciudad del Vaticano
+- Colombia
+- Comoras
+- Congo
+- Corea del Norte
+- Corea del Sur
+- Costa de Marfil
+- Costa Rica
+- Croacia
+- Cuba
+- Dinamarca
+- Dominica
+- Ecuador
+- Egipto
+- El Salvador
+- Emiratos Árabes Unidos
+- Eritrea
+- Eslovaquia
+- Eslovenia
+- España
+- Estados Unidos
+- Estonia
+- Etiopía
+- Filipinas
+- Finlandia
+- Fiyi
+- Francia
+- Gabón
+- Gambia
+- Georgia
+- Ghana
+- Granada
+- Grecia
+- Guatemala
+- Guinea
+- Guinea-Bisáu
+- Guinea Ecuatorial
+- Guyana
+- Haití
+- Honduras
+- Hungría
+- India
+- Indonesia
+- Irak
+- Irán
+- Irlanda
+- Islandia
+- Islas Marshall
+- Islas Salomón
+- Israel
+- Italia
+- Jamaica
+- Japón
+- Jordania
+- Kazajistán
+- Kenia
+- Kirguistán
+- Kiribati
+- Kosovo
+- Kuwait
+- Laos
+- Lesoto
+- Letonia
+- Líbano
+- Liberia
+- Libia
+- Liechtenstein
+- Lituania
+- Luxemburgo
+- Macedonia del Norte
+- Madagascar
+- Malasia
+- Malaui
+- Maldivas
+- Malí
+- Malta
+- Marruecos
+- Mauricio
+- Mauritania
+- México
+- Micronesia
+- Moldavia
+- Mónaco
+- Mongolia
+- Montenegro
+- Mozambique
+- Birmania
+- Namibia
+- Nauru
+- Nepal
+- Nicaragua
+- Níger
+- Nigeria
+- Noruega
+- Nueva Zelanda
+- Omán
+- Países Bajos
+- Pakistán
+- Palaos
+- Palestina
+- Panamá
+- Papúa Nueva Guinea
+- Paraguay
+- Perú
+- Polonia
+- Portugal
+- Reino Unido
+- República Centroafricana
+- República Checa
+- República del Congo
+- República Dominicana
+- Ruanda
+- Rumania
+- Rusia
+- Samoa
+- San Cristóbal y Nieves
+- San Marino
+- San Vicente y las Granadinas
+- Santa Lucía
+- Santo Tomé y Príncipe
+- Senegal
+- Serbia
+- Seychelles
+- Sierra Leona
+- Singapur
+- Siria
+- Somalia
+- Sri Lanka
+- Suazilandia
+- Sudáfrica
+- Sudán
+- Sudán del Sur
+- Suecia
+- Suiza
+- Surinam
+- Tailandia
+- Tanzania
+- Tayikistán
+- Timor Oriental
+- Togo
+- Tonga
+- Trinidad y Tobago
+- Túnez
+- Turkmenistán
+- Turquía
+- Tuvalu
+- Ucrania
+- Uganda
+- Uruguay
+- Uzbekistán
+- Vanuatu
+- Venezuela
+- Vietnam
+- Yemen
+- Yibuti
+- Zambia
+- Zimbabue
 `.trim()
 
   // mentions: mencionar al usuario que abrió el menú (opc.)
